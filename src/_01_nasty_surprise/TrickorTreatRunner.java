1dc7818ac66a68cdc398ce1fe4a46afc789bece8
League Student <league@iMac-23.attlocal.net>
2019-09-24 20:01:57 -0700

    NastySuprise

---
+++ src/_01_nasty_surprise/TrickorTreatRunner.java
@@ -0,0 +1,9 @@
+package _01_nasty_surprise;
+
+import javax.swing.SwingUtilities;
+ class TrickorTreatRunner {
+public static void main(String[] args) {
+NastySuprise n = new NastySuprise();
+n.run();
+}
+}
